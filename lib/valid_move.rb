f83f2b78f4d021b1e696196ebfa209871f17bf49
ktochow1 <ktochow1@binghamton.edu>
2018-08-16 01:38:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,9 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == 2.between?(1, 9)
     return true
+  elsif position_taken(board, index) == false
+    return true
+    
   else position_taken?(board, index) == true
     return false
 
