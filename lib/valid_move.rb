621f79ddc9d965af6857d79a1e5d4cf850a1e9a0
ktochow1 <ktochow1@binghamton.edu>
2018-08-16 01:34:49 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index) = false
+  if position_taken?(board, index) == false
     return true
-  else position_taken?(board, index) = true
+  else position_taken?(board, index) == true
     return false
 
 end
